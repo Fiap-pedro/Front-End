ae708baebe44152d4a1c83355cd9cf6c3b7e4923
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-09-18 22:46:20 -0300

    inserindo gemini no front

---
+++ aula12-gemini-bootstrap/gemini-back/teste.js
@@ -0,0 +1,14 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+//Instanciando o Gemini com a tua API KEY
+const genAI = new GoogleGenerativeAI("AIzaSyCnQrqSeD07HxC5M3YcKL6WetdbzuDtpKE");
+
+//Selecionando o modelo a ser utilizado
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+
+const prompt = "Qual o nome mais popular no Brasil ?";
+
+//resultado da requisição ao Gemini
+const result = await model.generateContent(prompt);
+console.log(result.response.text());
